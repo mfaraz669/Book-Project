76ee176a3a85cc2af207e21eb546d5beada9b4a0
mohamfa <mfaraz669@gmail.com>
2024-04-26 16:56:18 +0530

    from new system.

---
+++ Bodmas.java
@@ -0,0 +1,9 @@
+public class Bodmas {
+    public static void main(String[] args) {
+        int a = 9/3/3;
+        int b = 9*3;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
