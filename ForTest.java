90c3f36be54b887a2d7908caf33c4e5bf86c406d
faraz <mfaraz669@yahoo.com>
2024-04-26 10:54:05 +0530

    Initial commit

---
+++ ForTest.java
@@ -0,0 +1,10 @@
+public class ForTest {
+    public static void main(String[] args) {
+        long x;
+
+        for (x=0; x<10; x = x+1) {
+            System.out.println("This is x value: " + x);
+        }
+
+    }
+}
